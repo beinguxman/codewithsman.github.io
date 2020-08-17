453b079f301c6bbc7c8a03ab344ef0306a553e30
beinguxman <64712829+beinguxman@users.noreply.github.com>
2020-08-17 12:38:47 +0500

    PORTFOLIO

---
+++ index.php
@@ -0,0 +1,390 @@
+<!DOCTYPE html>
+<html lang="en" id="top">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge" />
+    <meta name="viewport" content="width=device-width" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>USMAN | Web Designer nd Developer</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <link type="text/css" rel="stylesheet" href="css/bootstrap.css" />
+    <link type="text/css" rel="stylesheet" href="style.css">
+</head>
+
+<body onload="myFunction()">
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script src="typed.js"></script>
+    <div id="loading"></div>
+        <section class="header-div0" id="Home">
+            <nav class="navbar navbar-expand-md mm-auto navbar-dark bg-dark fixed-top">
+                <a class="navbar-brand" href="#">CODEWITHUSMAN</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#Home">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#SERVICES">SERVICES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#WORK">WORK</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#PROGRESS">PROGRESS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#CONTACT">CONTACT</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <header class="masthead">
+                <div class="container h-100">
+                  <div class="row h-100 align-items-center">
+                    <div class="col-4 text-center">
+                      <h1 class="font-weight-heavy " style="color:white;"> HI,THIS IS USMAN</h1>
+                      <!-- <h2 class="font-weight-heavy"  style="color:white;">A FULL STACK Developer<br>GRAPHICS DESIGNER</h2> -->
+                      <div id="text"  style="color:white;"></div>
+                      <br>
+                      <div class="banner-area">
+                        <h1><span style="color:white;">I'm a  </span><span class="type" style="color: red;"></span></h1>
+                    </div> 
+                    </div>
+                  </div>
+                </div>         
+            <!-- <div class="header-div">
+                <h1>Hi,This is <span
+                        style="color:black; font-weight: bold; font-style: italic; font-size: 50px;">Usman
+                        Ahmad</span><br>A Full Stack Developer<br>And Graphics Designer</bt>
+                    <button type="button" class="btn btn-light"><a href="#SERVICES">SERVICES</a></button>
+                    <button type="button" class="btn btn-light1"><a href="#CONTACT">HIRE ME</a></button>
+            </div> -->
+            </header>
+    </section>
+    <hr>
+    <section class="header-div1" id="SERVICES">
+        <h1>SERVICES</h1>
+        <div class="divider-custom">
+            <div class="divider-custom-line"></div>
+            <div class="divider-custom-icon"><i class="fa fa-cog" aria-hidden="true"></i></div>
+            <div class="divider-custom-line"></div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="extra-div col-lg-4 col-md-4 col-12">
+                    <a href="#SERVICES"><i class="fa-2x fa fa-thermometer-full" aria-hidden="true"></i></a>
+                    <h2>Dynamic</h2>
+                    <p>Websites don't always have to be static. You see,
+                        those cool custom Content Management Systems
+                        (CMS) that allow you to change your websites
+                        content yourself, I can develop them for you.
+                    </p>
+                </div>
+                <div class="extra-div col-lg-4 col-md-4 col-12">
+                    <a href="#SERVICES"> <i class="fa fa-window-restore" aria-hidden="true"></i></a>
+                    <h2>Responsive Design</h2>
+                    <p>Never be worried about responsiveness as all my
+                        designs and websites are responsive. They are
+                        checked thoroughly on different screen sizes and
+                        adjusted accordingly.</p>
+                </div>
+                <div class="extra-div col-lg-4 col-md-4 col-12">
+                    <a href="#SERVICES"><i class="fa-2x fa fa-shield" ria-hidden="true"></i></a>
+                    <h2>UX/UI</h2>
+                    <p>Who does not like those fancy, magical and easy
+                        to use UI designs? I do and if you want one for
+                        yourself, I can design one for you.</p>
+                </div>
+                <div class="extra-div col-lg-4 col-md-4 col-12">
+                    <a href="#SERVICES"><i class="fa-2x fa fa-paint-brush" aria-hidden="true"></i></a>
+                    <h2>Dynamic</h2>
+                    <p>Websites don't always have to be static. You see,
+                        those cool custom Content Management Systems
+                        (CMS) that allow you to change your websites
+                        content yourself, I can develop them for you.
+                    </p>
+                </div>
+                <div class="extra-div col-lg-4 col-md-4 col-12">
+                    <a href="#SERVICES"><i class="fa-2x fa fa-thermometer-full" aria-hidden="true"></i></a>
+                    <h2>Responsive Design</h2>
+                    <p>Never be worried about responsiveness as all my
+                        designs and websites are responsive. They are
+                        checked thoroughly on different screen sizes and
+                        adjusted accordingly.</p>
+                </div>
+                <div class="extra-div col-lg-4 col-md-4 col-12">
+                    <a href="#SERVICES"><i class="fa-2x fa fa-shield" aria-hidden="true"></i></a>
+                    <h2>UX/UI</h2>
+                    <p>Who does not like those fancy, magical and easy
+                        to use UI designs? I do and if you want one for
+                        yourself, I can design one for you.</p>
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <hr>
+    </section>
+    <section class="header-div2" id="WORK">
+        <h1>PORTFOLIO</h1>
+        <div class="divider-custom">
+            <div class="divider-custom-line"></div>
+            <div class="divider-custom-icon"><i class="fa fa-cog" aria-hidden="true"></i></div>
+            <div class="divider-custom-line"></div>
+        </div>
+        <br>
+        <div class="container">
+            <div class="row">
+                <video class="video-fluid z-depth-1" autoplay loop controls muted controls width="320" height="240">
+                    <source src="https://mdbootstrap.com/img/video/Sail-Away.mp4" type="video/mp4" />
+                </video>
+                <div class="portfolio-items col-sm-4">
+                    <img src="img1.png" alt="..." class="img-thumbnail">
+                    <h2>Dynamic</h2>
+                </div>
+                <div class="portfolio-items col-sm-4">
+                    <img src="img1.png" alt="..." class="img-thumbnail">
+                    <h2>Dynamic</h2>
+                </div>
+                <div class="portfolio-items col-sm-4">
+                    <img src="img1.png" alt="..." class="img-thumbnail">
+                    <h2>Dynamic</h2>
+                </div>
+                <div class="portfolio-items col-sm-4">
+                    <img src="img1.png" alt="..." class="img-thumbnail">
+                    <h2>Dynamic</h2>
+                </div>
+                <div class="portfolio-items col-sm-4">
+                    <img src="img1.png" alt="..." class="img-thumbnail">
+                    <h2>Dynamic</h2>
+                </div>
+                <div class="portfolio-items col-sm-4">
+                    <img src="img1.png" alt="..." class="img-thumbnail">
+                    <h2>Dynamic</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <hr>
+    <section class="header-div3" id="PROGRESS">
+        <div class="container">
+            <h1>What I'm Offering</h1>
+            <div class="divider-custom">
+                <div class="divider-custom-line"></div>
+                <div class="divider-custom-icon"><i class="fa fa-handshake-o" aria-hidden="true"></i></div>
+                <div class="divider-custom-line"></div>
+            </div>
+            <div class="row">
+                <div class="About col-sm-4">
+                    <center><img src="about.png" alt="..." width="150px" height="150px" class="img-thumbnail"></center>
+                    <br>
+                    <div class="About-body">
+                        <h5>ABOUT</h5>
+                        <p class="About-text">ASSALAM O ALAIKUM,This is me Usman Ahmad i'm professional Web Developer and Designer and Also run a software House with a professional Team of Graphics and WEB as Well as gaming and Andriod  apps </p>
+                    </div>
+                    <div>
+                    <i class="fa-2x fa fa-envelope" aria-hidden="true"> Uawan203@gmail.com</i>
+                    <br><br>
+                    <i class="fa-2x fa fa-whatsapp" aria-hidden="true">   +92300-4966637</i>    
+                    </div>
+                </div>
+                <div class="Progress col-sm-8">
+                    <h2>Skills Experties</h2>
+                    <br>
+                    <dt>HTML</dt>
+                    <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0"
+                        aria-valuemax="100">80% </div>
+                    <dt>css</dt>
+                    <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="90"
+                        aria-valuemin="0" aria-valuemax="100">90%</div>
+                    <dt>scss</dt>
+                    <div class="progress-bar" role="progressbar" style="width: 85%;" aria-valuenow="85"
+                        aria-valuemin="0" aria-valuemax="100">85%</div>
+                    <dt>PHP</dt>
+                    <div class="progress-bar" role="progressbar" style="width: 65%;" aria-valuenow="65"
+                        aria-valuemin="0" aria-valuemax="100">65%</div>
+                    <dt>REACT JS</dt>
+                    <div class="progress-bar" role="progressbar" style="width: 55%;" aria-valuenow="55"
+                        aria-valuemin="0" aria-valuemax="100">55%</div>
+                    <dt>Angular 6</dt>
+                    <div class="progress-bar" role="progressbar" style="width: 80%;" aria-valuenow="80"
+                        aria-valuemin="0" aria-valuemax="100">80%</div>
+                    <dt>WORDPRESS</dt>
+                    <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="90"
+                    aria-valuemin="0" aria-valuemax="100">90%</div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+    <hr>
+    <section class="contact" id=CONTACT>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $desc = $_POST['desc'];
+        
+      
+      // Connecting to the Database
+      $servername = "localhost";
+      $username = "root";
+      $password = "";
+      $database = "contacts";
+
+      // Create a connection
+      $conn = mysqli_connect($servername, $username, $password, $database);
+      // Die if connection was not successful
+      if (!$conn){
+          die("Sorry we failed to connect: ". mysqli_connect_error());
+      }
+      else{ 
+        // Submit these to a database
+        // Sql query to be executed 
+        $sql = "INSERT INTO `contactus` (`name`, `email`, `concern`, `dt`) VALUES ('$name', '$email', '$desc', current_timestamp())";
+        $result = mysqli_query($conn, $sql);
+ 
+        if($result){
+        '<div class="alert alert-success alert-dismissible fade show" role="alert">
+          <strong>Success!</strong> Your entry has been submitted successfully!
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>';
+        }
+        else{
+            // echo "The record was not inserted successfully because of this error ---> ". mysqli_error($conn);
+         '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+          <strong>Error!</strong> We are facing some technical issue and your entry ws not submitted successfully! We regret the inconvinience caused!
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>';
+        }
+
+      }
+
+    }
+?>
+
+<div class="container mt-3">
+<h1>Contact us for your concerns</h1>
+    <form action="index.php" method="post">
+    <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" name="name" class="form-control" id="name" aria-describedby="emailHelp">
+    </div>
+
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp"> 
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+
+    <div class="form-group">
+        <label for="desc">Description</label>
+        <textarea class="form-control" name="desc" id="desc" cols="30" rows="10"></textarea> 
+    </div>
+    
+    <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+        <br>
+
+        <!-- <section class="footer"> -->
+             <!-- Site footer -->
+    <footer class="site-footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12 col-md-6">
+              <h6>About</h6>
+              <p class="text-justify">CODEWITHUSHMAN<i>  Hi, This is Usman.</i> i'm professional Web Developer and Designer and Also prorvide Graphics Services and Website Services With best UX-Design. I focuses on providing the most efficient Websites of your need. I have ability to  build up  in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL, Angular, React js, Node Js and also have abilty to make algos.</p>
+            </div>
+            <div class="col-xs-6 col-md-3">
+              <h6>Categories</h6>
+              <ul class="footer-links">
+                <li><a href="">WEb Developer</a></li>
+                <li><a href="">UI Design</a></li>
+                <li><a href="">Graphics Design</a></li>
+                <li><a href="">Android</a></li>
+              </ul>
+            </div>
+  
+            <div class="col-xs-6 col-md-3">
+              <h6>Quick Links</h6>
+              <ul class="footer-links">
+                <li><a href="#Home">Home</a></li>
+                <li><a href="#SERVICES">Services</a></li>
+                <li><a href="#WORK">Work</a></li>
+                <li><a href="#PROGRESS">Progress</a></li>
+                <li><a href="#CONTACT">Contact</a></li>
+                <li><a href="#top">Jump to top of page</a></li>
+              </ul>
+            </div>
+          </div>
+          <hr>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8 col-sm-6 col-xs-12">
+              <p class="copyright-text">Copyright &copy; 2020 All Rights Reserved by 
+           <a href="#">Usman Ahmad</a>.
+              </p>
+            </div>
+  
+            <div class="col-md-4 col-sm-6 col-xs-12">
+              <ul class="social-icons">
+                <li><a class="facebook" href="https://www.facebook.com/usman.awan41"><i class="fa fa-facebook"></i></a></li>
+                <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+                <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+              </ul>
+            </div>
+          </div>
+        </div>
+  </footer>
+        <script>
+            // $(document).ready(function(){
+            // 	$('div#loading').removeAttr('id');
+            // });
+            var preloader = document.getElementById("loading");
+            // window.addEventListener('load', function(){
+            // 	preloader.style.display = 'none';
+            // 	})
+
+            function myFunction() {
+                preloader.style.display = 'none';
+            };
+        </script>
+        <script type="text/javascript">
+            var i = 0, text;
+            text = "Welcome To CODEWITHUSMAN"
+            function typing() {
+                if (i < text.length) {
+                    document.getElementById("text").innerHTML += text.charAt(i);
+                    i++;
+                    setTimeout(typing, 90);
+                }
+            }
+            typing();
+        </script>
+        <script>
+            var typed = new Typed('.type', {
+        strings: ['WEB Developer', 'GRAPHICS Designer', ' Freelancer'],
+        typeSpeed: 60,
+        backSpeed: 60,
+        loop: true
+        });
+        </script>
+            
+</body>
+
+</html>
